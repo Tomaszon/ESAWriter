822a80d59ab8fc030f81d5908f9127602393ce8a
Benkó Tamás <benko.tamas@gammadigital.hu>
2020-01-29 16:47:44 +0100

    01/29/20

--- ESAWriter/ESAWriter/MainPage.xaml.cs
+++ ESAWriter/ESAWriter/MainPage.xaml.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using ESAWriter.ViewModels;
+using System;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ESAWriter
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPageViewModel Model = new MainPageViewModel();
+		private readonly Random r = new Random();
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonTest_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			Model.Property2 = r.Next(10000);
+		}
+	}
 }
---
+++ ESAWriter/ESAWriter/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ESAWriter.ViewModels
+{
+	public class MainPageViewModel
+	{
+		public string Property1 { get; set; } = "Some text";
+
+		public int Property2 { get; set; } = 12345;
+	}
+}
