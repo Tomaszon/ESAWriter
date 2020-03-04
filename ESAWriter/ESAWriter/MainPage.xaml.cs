8b512d63492b2e5a19757db8d5aecb87cca43823
Benkó Tamás <benko.tamas@gammadigital.hu>
2020-03-04 16:46:20 +0100

    viewModel, model experiments

--- ESAWriter/ESAWriter/MainPage.xaml.cs
+++ ESAWriter/ESAWriter/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using ESAWriter.Models;
-using ESAWriter.ViewModels;
 using System;
+using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -10,25 +13,49 @@ namespace ESAWriter
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class MainPage : Page
+	public sealed partial class MainPage : ESABasePage<SampleModel, SampleViewModel>
 	{
 		//public MainPageViewModel Model = new MainPageViewModel();
 		private readonly Random r = new Random();
 
-		public Class Class = new Class();
+		private readonly Polygon polygon = new Polygon();
 
-		public MainPage()
+		public MainPage() : base()
 		{
 			InitializeComponent();
+
+			polygon.Points.Add(new Point(100, 100));
+			polygon.Points.Add(new Point(200, 100));
+			polygon.Points.Add(new Point(250, 200));
+			polygon.Points.Add(new Point(100, 200));
+
+			polygon.Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+			polygon.StrokeThickness = 2;
+
+			polygon.Fill = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+
+			canvas1.Children.Add(polygon);
 		}
 
 		private void ButtonTest_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
 		{
 			//Model.Property2 = r.Next(10000);
 
-			Class.A = r.Next(10000);
+			Model.A = r.Next(10000);
+		}
+	}
 
+	public abstract class ESABasePage<TModel, TViewModel> : Page where TViewModel : ViewModelBase where TModel: ModelBase
+	{
+		public ModelBase Model { get; }
 
+		public ViewModelBase ViewModel { get; }
+
+		public ESABasePage()
+		{
+			Model = new TModel();
+
+			ViewModel = new TViewModel();
 		}
 	}
 }
---
+++ ESAWriter/ESAWriter/Models/SampleViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ESAWriter.Models
+{
+	public class SampleViewModel : ViewModelBase
+	{
+		public ViewModelProperty<string, int> A { get { return A.Get(); } set { Set(A, value); } }
+
+		public SampleViewModel(ModelBase model) : base(model)
+		{
+			InitProperty(A, nameof(A), p => $"Formatted: {p}");
+		}
+	}
+
+	public class ViewModelBase : INotifyPropertyChanged
+	{
+		private readonly ModelBase _model;
+
+		public ViewModelBase(ModelBase model)
+		{
+			_model = model;
+		}
+
+		protected void InitProperty<TGet, TSet>(ViewModelProperty<TGet, TSet> property, string modelPropertyName, Func<TSet, TGet> format)
+		{
+			property = new ViewModelProperty<TGet, TSet>(_model, modelPropertyName, format);
+		}
+
+		protected void Set<TGet, TSet>(ViewModelProperty<TGet, TSet> property, TSet value, [CallerMemberName]string name = null)
+		{
+			property.Set(value);
+
+			OnPropertyChanged(name);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName]string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+
+	public class SampleModel : ModelBase
+	{
+		public int A { get; set; }
+	}
+
+	public class ModelBase
+	{
+		public T GetProperty<T>(string propertyName)
+		{
+			return (T)GetType().GetProperty(propertyName).GetValue(this);
+		}
+
+		public void SetProperty<T>(string propertyName, T value)
+		{
+			GetType().GetProperty(propertyName).SetValue(this, value);
+		}
+	}
+
+	public class ViewModelProperty<TGet, TSet>
+	{
+		private TGet _getValue;
+
+		private TSet _setValue;
+
+		private readonly ModelBase _model;
+
+		private readonly string _propertyName;
+
+		private readonly Func<TSet, TGet> _format;
+
+		public ViewModelProperty() { }
+
+		public ViewModelProperty(ModelBase model, string propertyName, Func<TSet, TGet> format)
+		{
+			_model = model;
+			_propertyName = propertyName;
+			_format = format;
+		}
+
+		public static implicit operator ViewModelProperty<TGet, TSet>(TSet value)
+		{
+			return new ViewModelProperty<TGet, TSet>() { _setValue = value };
+		}
+
+		//for setter only
+		public static implicit operator TSet(ViewModelProperty<TGet, TSet> property)
+		{
+			return property._setValue;
+		}
+
+		public static implicit operator TGet(ViewModelProperty<TGet, TSet> property)
+		{
+			return property.Get();
+		}
+
+		//for getter only
+		public static implicit operator ViewModelProperty<TGet, TSet>(TGet value)
+		{
+			return new ViewModelProperty<TGet, TSet>() { _getValue = value };
+		}
+
+		public void Set(TSet value)
+		{
+			_model.SetProperty(_propertyName, value);
+		}
+
+		public TGet Get()
+		{
+			_getValue = _model.GetProperty<TGet>(_propertyName);
+			return _getValue;
+		}
+	}
+}
